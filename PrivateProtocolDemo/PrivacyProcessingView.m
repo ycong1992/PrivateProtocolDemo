e4feb0aa5e5a753167ab5337aa2f9772a3ea1978
ycong1992 <709932492@qq.com>
2020-03-02 17:14:49 +0800

    modify

--- PrivateProtocolDemo/PrivacyProcessingView.m
+++ PrivateProtocolDemo/PrivacyProcessingView.m
@@ -8,14 +8,60 @@
 
 #import "PrivacyProcessingView.h"
 
+@interface PrivacyProcessingView ()
+
+@property (nonatomic, strong) UIImageView *bgImage;
+@property (nonatomic, strong) UILabel *detailLabel;
+@property (nonatomic, strong) UIButton *confirmBtn;
+@property (nonatomic, strong) UIButton *cancelBtn;
+
+@end
+
 @implementation PrivacyProcessingView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self initUI:frame];
+    }
+    return self;
 }
-*/
+
+- (void)initUI:(CGRect)frame {
+    self.bgImage = [[UIImageView alloc] initWithFrame:frame];
+    self.bgImage.image = [UIImage imageWithContentsOfFile:[[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"StartImage.png"]];
+    [self addSubview:self.bgImage];
+
+    self.detailLabel = [[UILabel alloc] initWithFrame:CGRectMake(15, 0, CGRectGetWidth(frame)-30, 300)];
+    self.detailLabel.text = @"隐私协议xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    self.detailLabel.numberOfLines = 0;
+    [self addSubview:self.detailLabel];
+    
+    self.confirmBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.confirmBtn.frame = CGRectMake(15, CGRectGetMaxY(self.detailLabel.frame)+15, CGRectGetWidth(frame)-30, 48);
+    self.confirmBtn.backgroundColor = [UIColor blueColor];
+    [self.confirmBtn setTitle:@"同意" forState:UIControlStateNormal];
+    [self.confirmBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.confirmBtn addTarget:self action:@selector(confirmBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:self.confirmBtn];
+    
+    self.cancelBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.cancelBtn.frame = CGRectMake(15, CGRectGetMaxY(self.confirmBtn.frame)+15, CGRectGetWidth(frame)-30, 48);
+    self.cancelBtn.backgroundColor = [UIColor blueColor];
+    [self.cancelBtn setTitle:@"不同意" forState:UIControlStateNormal];
+    [self.cancelBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [self.cancelBtn addTarget:self action:@selector(cancelBtnClick) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:self.cancelBtn];
+}
+
+- (void)confirmBtnClick {
+    [self removeFromSuperview];
+//    self = nil;
+    //  [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
+}
+
+- (void)cancelBtnClick {
+    exit(0);
+}
 
 @end
--- PrivateProtocolDemo/ViewController.m
+++ PrivateProtocolDemo/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "PrivacyProcessingView.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) PrivacyProcessingView *privacyView;
 
 @end
 
@@ -17,6 +20,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(50, 100, 300, 100)];
+    label.text = @"测试";
+    [self.view addSubview:label];
+    
+    // test
+//    if (@available(iOS 13.0, *)) {
+//      [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDarkContent animated:NO];
+//    } else {
+//      [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleDefault];
+//    }
+    [self.view addSubview:self.privacyView];
+}
+
+- (PrivacyProcessingView *)privacyView {
+    if (!_privacyView) {
+        _privacyView = [[PrivacyProcessingView alloc] initWithFrame:self.view.frame];
+    }
+    return _privacyView;
 }
 
 
