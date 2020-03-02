363dc29e6fea482c8fed15c2a3788fd3976fcfb1
xieyuecong <xieyuecong@0easy.com>
2020-03-02 17:04:27 +0800

    first commit

---
+++ PrivateProtocolDemo/PrivacyProcessingView.m
@@ -0,0 +1,21 @@
+//
+//  PrivacyProcessingView.m
+//  PrivateProtocolDemo
+//
+//  Created by SZOeasy on 2020/3/2.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import "PrivacyProcessingView.h"
+
+@implementation PrivacyProcessingView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
