0083ddea6a15a9c22d3cffd312d90d7e3a523bd9
xieyuecong <xieyuecong@0easy.com>
2020-03-02 16:24:17 +0800

    Initial Commit

---
+++ PrivateProtocolDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrivateProtocolDemo
+//
+//  Created by SZOeasy on 2020/3/2.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PrivateProtocolDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  PrivateProtocolDemo
+//
+//  Created by SZOeasy on 2020/3/2.
+//  Copyright © 2020 ycong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
